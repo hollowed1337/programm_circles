d702031da3b4d2ac2da4a9e6c21f354827a87d77
hollowed1337 <saha011p@gmail.com>
2023-01-20 11:59:45 +0300

    lab_5 updated

---
+++ migrations/versions/lab5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: lab5
+Revises: cd4b272c9697
+Create Date: 2023-01-20 11:25:57.461836
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'lab5'
+down_revision = 'cd4b272c9697'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
